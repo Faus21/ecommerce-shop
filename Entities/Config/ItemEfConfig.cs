75d97e295bba5b8e998ad3aa09363cf6f15895b7
Mykyta <fausgaming21@gmail.com>
2025-02-11 23:45:51 +0100

    'Initial'

---
+++ Entities/Config/ItemEfConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Entities.Config
+{
+    public class ItemEfConfig : IEntityTypeConfiguration<Item>
+    {
+        public void Configure(EntityTypeBuilder<Item> builder)
+        {
+            builder.HasKey(e => e.Id).HasName("Item_pk");
+
+            builder.Property(e => e.Name).HasMaxLength(100).IsRequired();
+            builder.Property(e => e.Price).HasConversion(typeof(decimal));
+            
+
+            builder.ToTable("Item");
+
+            Item[] items =
+            {
+                new Item
+                {
+                    Id = 1,
+                    Name = "Box",
+                    Price = 20m
+                }
+            };
+
+            builder.HasData(items);
+        }
+    }
+}
